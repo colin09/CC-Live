c609fb0225e40ec2aa67bbddb4e738da6324f9be
duxl <duxinglong@yijiupi.com>
2021-08-27 08:53:58 +0800

    test

---
+++ C.L.Server/test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using c.l.common.helper;
+
+namespace test {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+
+            var productSkuIds = new string[] { "aaaa", "bbbbb" };
+            var request = new List<dynamic> ();
+            request.Add (productSkuIds);
+            request.Add (1123);
+            System.Console.WriteLine (request.ToJson ());
+
+            var response = new Dictionary<string, List<string>> ();
+            response.Add ("1234", productSkuIds.ToList());
+            System.Console.WriteLine (response.ToJson ());
+        }
+    }
+}
